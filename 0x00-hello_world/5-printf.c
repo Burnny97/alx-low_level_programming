69d3d35cc4da0678c4c6a7b5396db862f3233247
Burnny97 <nburnny9752@gmail.com>
2022-07-04 18:12:28 -0700

    Hello, world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
 #!/bin/bash
-Hello, printf
+#include<stdio.h>
+/
+*main-Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+print("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
