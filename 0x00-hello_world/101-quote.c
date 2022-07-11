aff70c7d085e182e7df8a5b252e5e16c380ff8b6
Burnny97 <nburnny9752@gmail.com>
2022-07-11 04:52:45 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main -Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	fprintf(
-			stderr, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n");
+			stderr, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n"
+			);
 	return (1);
 }
