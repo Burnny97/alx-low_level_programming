c0739573efeda4013d8731ddb05c7e417614d00e
Burnny97 <nburnny9752@gmail.com>
2022-07-12 07:32:13 -0700

    Points

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -20,8 +20,8 @@ void rev_string(char *s)
 		{
 			for (j = i + 1; j > 0; j--)
 			{
-				temp = *(s + j)
-				* (s + j) = *(s + (j - 1));
+				temp = char
+				char = *(s + (j - 1));
 				*(s + (j - 1)) = temp;
 			}
 		}
