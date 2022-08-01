2d838b164075add82a74fee81abd444e8290a704
Burnny97 <nburnny9752@gmail.com>
2022-08-01 05:19:34 -0700

    Win

--- 0x0c-mmv/0-malloc_checked.c
+++ 0x0c-mmv/0-malloc_checked.c
@@ -10,9 +10,10 @@
 void *malloc_checked(unsigned int b)
 {
 	char *p;
-
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
-	return (p);
+	{
+		p = malloc(b);
+		if (p == NULL)
+			exit(98);
+		return (p);
+	}
 }
